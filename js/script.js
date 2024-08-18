9d6f604ff14555f6ab1392138ab7d92833bef96e
UsherQelich SayyedMuhammadi <faslolkhitab@gmail.com>
2024-08-18 12:49:26 +0330

    se02

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
-// alert(document.getElementById("myContent").innerHTML);
-console.log('welcome')
+const screen = document.getElementById("demo");
+screen.style.fontSize = "58px";
+let x = 5,
+  y = 6,
+  z = 21;
+let res = "";
+console.log(res);
+//   const firstName = "Usher";
+//   const lastName = "SM";
+//   screen.innerHTML = firstName + " " + lastName;
+// screen.innerHTML = "1" + x + y  + z;
+res = res + x + "<br>";
+res = res + (x + y) + "<br>";
+res = res + (x + y + z);
+screen.innerHTML = res;
+{
+  let x = 50;
+  screen.innerHTML = x;
+  console.log(x);
+}
+function showX() {
+  let x = 60;
+  console.log(x);
+}
+showX();
+console.log(x);
+screen.innerHTML = 100;
