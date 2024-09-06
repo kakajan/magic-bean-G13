9bc68cf92aac5aa69b25f5785e2520dcc7e63c29
UsherQelich SayyedMuhammadi <faslolkhitab@gmail.com>
2024-09-06 13:12:02 +0330

    E05-PartA

--- E05/js/calc.js
+++ E05/js/calc.js
@@ -1,13 +1,52 @@
 const screen = document.querySelector(".screen");
-let screenNumber = "0";
+
+let screenNumber = "0",
+  partA,
+  partB,
+  operatorType,
+  helper = 0;
+
 screen.innerHTML = screenNumber;
-function printOnScreen() {
-  screen.innerHTML = (+screenNumber).toLocaleString();
+
+function printOnScreen(x) {
+  if (x === ".") {
+    screen.innerHTML = (+screenNumber).toLocaleString() + ".";
+  } else {
+    screen.innerHTML = (+screenNumber).toLocaleString();
+  }
 }
-function setNum (x) {
-    if (screenNumber === '0') {
-        screenNumber = ''
-    }
+
+function setNum(x) {
+  if (helper) {
+    screenNumber = "";
+    helper = 0;
+  }
+  if (screenNumber === "0") {
+    screenNumber = "";
+  }
   screenNumber = screenNumber + x;
-  printOnScreen();
+  printOnScreen(x);
+}
+
+function operate(type) {
+  partA = +screenNumber;
+  operatorType = type;
+  console.log(partA);
+  helper = 1;
+}
+
+function calculate() {
+  partB = +screenNumber;
+
+  if (operatorType === "+") {
+    result = partA + partB;
+  } else if (operatorType === "-") {
+    result = partA - partB;
+  } else if (operatorType === "*") {
+    result = partA * partB;
+  } else {
+    result = partA / partB;
+  }
+
+  screen.innerHTML = result;
 }
